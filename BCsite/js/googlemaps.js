171715a062f2211962f9c6f32ab216346456a73f
Rich <richardcibu@yahoo.com>
2016-10-12 21:51:22 -0400

    changed googlemaps.js again to allow for user localization of initial map

--- BCsite/js/googlemaps.js
+++ BCsite/js/googlemaps.js
@@ -1,15 +1,36 @@
 var test = [];
 
 function initMap() {
-  var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new google.maps.DirectionsRenderer;
-
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: {lat: 40.744194, lng: -73.994052},
   });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
-  directionsDisplay.setMap(map);
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
 }
 
 function initMap2() {
@@ -59,7 +80,7 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
 
   console.log(origin);
   console.log(destination);
-  
+
   directionsService.route({
     origin: origin,
     destination: destination,
